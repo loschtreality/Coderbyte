d083f71b19ec62c5263457623f52f35f073553d1
Michael Loschiavo <losch103@gmail.com>
2016-03-06 22:34:38 -0800

    touched up

--- Beginner_Challenges/ArrayAddition1.js
+++ Beginner_Challenges/ArrayAddition1.js
@@ -1,13 +1,15 @@
 // Using the JavaScript language, have the function ArrayAdditionI(arr) take the array of numbers stored in arr and return the string true if any combination of numbers in the array can be added up to equal the largest number in the array, otherwise return the string false. For example: if arr contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all the same elements, and may contain negative numbers.
 
-function arrayAdd(array) {
-array = Array.prototype.slice.call(arguments);
+function ArrayAdditionI(array) {
 var sortedArray = array.sort(function(a,b){return a-b;});
 var greatestNumber = sortedArray.pop();
 var allSummed = sortedArray.reduce(function(prev,curr){return prev + curr;});
 var difference;
   if (allSummed < greatestNumber) {
     return false;
+  }
+  if (allSummed === greatestNumber) {
+    return true;
   }
 
   difference = allSummed - greatestNumber;
@@ -25,6 +27,6 @@ var difference;
 }
 
 
-console.log(arrayAdd(1,2,3,4));//true
-//console.log(arrayAdd([ 5,7,16,1,2]));//false
-//console.log(arrayAdd([3,5,-1,8,12]));//true
+console.log(arrayAdd([10,20,30,40,100]));//true
+console.log(arrayAdd([31,2,90,50,7]));//true
+console.log(arrayAdd([3,5,-1,8,12]));//true
