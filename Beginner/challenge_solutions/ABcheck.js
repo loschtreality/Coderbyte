ade59413188cb22fe1defc18e7be12d0205fb4be
Michael Loschiavo <losch103@gmail.com>
2016-04-14 10:52:58 -0700

    Improved my initial solution and reduced time complexity from O(n) to O(1)

--- Beginner/challenge_solutions/ABcheck.js
+++ Beginner/challenge_solutions/ABcheck.js
@@ -1,20 +1,16 @@
+// Using the JavaScript language, have the function ABCheck(str) take the str parameter being passed and return the string true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b). Otherwise return the string false.
+
+
 function ABCheck(str) {
-  for (var i = 0; i < str.length; i++) {
-    if(str[i].toLowerCase() === 'a' && str[i+4].toLowerCase() === 'b'){
-      return true;
-    }
-  }
+  str = str.toLowerCase();
+if (str.indexOf('a') === -1 || str.indexOf('b') == -1) {
   return false;
 }
+  return Math.abs(str.indexOf('a') - str.indexOf('b'))-1 === 3 || Math.abs(str.lastIndexOf('a') - str.lastIndexOf('b'))-1 === 3;
+}
+
 
 console.log(ABCheck('after baldy'));
-
-
-//How i should have done it:
-
-// function ABCheck(str) {
-//
-//
-//   return str.match(/(a[\s\S][\s\S][\s\S]b)|(b[\s\S][\s\S][\s\S]a)/i)!==null;
-//
-// }
+console.log(ABCheck("lane borrowed"));
+console.log(ABCheck("Laura sobs"));
+console.log(ABCheck("aaaaddddd"));
